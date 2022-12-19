03af52d4c7ece61a97569cd2eef04f1b7718aca1
nappy <Waffle>
2022-12-20 00:31:09 +0500

    add 'create' method for 'Payment' table class

--- models/payment.py
+++ models/payment.py
@@ -1,7 +1,12 @@
+from typing import Any
+from loguru import logger
 from datetime import datetime
 
+from sqlalchemy import insert
+from sqlalchemy.orm import scoped_session
 from sqlalchemy_utils.types import ChoiceType
 from sqlalchemy import Column, String, DateTime, VARCHAR, BigInteger
+
 
 from models import Base
 
@@ -25,9 +30,20 @@ class Payment(Base):
     is_paid = Column(ChoiceType(PAYMENT_STATE, impl=String()), default="В ожидании")
     order_id = Column(VARCHAR(50), nullable = False)
     order_link = Column(String, nullable = False)
-    creator_data = Column(VARCHAR(100), nullable = False)  # Format: user_id|phone|username
+    creator_data = Column(VARCHAR(100), nullable = False)   # Format: user_id|username
 
     lesson_type = Column(VARCHAR(20), nullable = False)
     parents_name = Column(VARCHAR(50), nullable = False)
     description = Column(VARCHAR(512))
     amount = Column(BigInteger, nullable = False)
+
+    @classmethod
+    async def create(cls, session: scoped_session, **kwargs: Any):
+        try:
+            query = insert(cls).values(**kwargs).returning(cls.id)
+            result = await session.execute(query)            
+            await session.commit()                          # type: ignore
+            return result.fetchone()[0]
+        except Exception as e:
+            logger.error("ROLLBACK EXCEPTION:", e, kwargs)
+            return
